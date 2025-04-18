2a1eca07b2a90abe3baf6a14d99857599f1a0272
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-04-18 22:54:29 +0600

    Mainlayout and Sidebar layout has been made for Dashboard

---
+++ resources/js/Pages/Layouts/Mainlayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Sidebar from './Sidebar.vue';
+</script>
+<template>
+    <div class="flex h-screen">
+        <!-- Sidebar -->
+        <Sidebar />
+
+        <!-- Main Content -->
+        <slot/>
+    </div>
+</template>
+
+
+
+<style>
+
+</style>
